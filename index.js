059464c00b9f5d18e58b70aa63674e1731a6fac7
plinnlatt <plinnlatt70@gmail.com>
2021-12-16 15:49:03 +0630

    node app dcocker

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get("/",(req,res)=>{
+    res.send("<h2>Hi Hereaa</h2>")
+})
+
+
+app.listen(port, ()=>console.log(`listening on port ${port}`))
