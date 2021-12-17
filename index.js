463916b713e6cf2ffd45c09969e3cd512c12e872
plinnlatt <plinnlatt70@gmail.com>
2021-12-18 02:42:39 +0630

    Controlling env for mongo connection
    -separate env of mongodb
    -make connection for retry till success
    -docker-compose||.dev updates

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
 const port = process.env.PORT || 3000
+const { MONGO_IP,MONGO_PORT,MONGO_USER,MONGO_PASSWORD } = require('./config/config')
+const mongourl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const mongodbWithRetry = ()=>{
+    mongoose
+    .connect(mongourl,{
+    useNewUrlParser: true,
+    })
+    .then(()=>console.log("db connected"))
+    .catch((err)=>{
+    console.log(err)
+    setTimeout(mongodbWithRetry,5000)
+    })
+}
+
+mongodbWithRetry()
 
 app.get("/",(req,res)=>{
     res.send("<h2>Hi Hereaa</h2>")
