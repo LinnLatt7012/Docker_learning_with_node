a762b38ccf2b42a7898c45d400f4c1cede18069a
plinnlatt <plinnlatt70@gmail.com>
2022-01-11 01:06:12 +0630

    Replicas and restart policy added to node app
    -docker-compse file update
    -made update for swarm

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.enable("trust proxy")
 app.get("/api/v1",(req,res)=>{
     console.log("it work!!!!")
-    res.send("<h1>it ran</h1>")
+    res.send("<h1>it work!!!!</h1>")
 })
 app.use("/api/v1/user",userRouter);
 
